d9757ba009f83024c84be79f4763fe7808b204d2
fridayfang <fridayfang@zhaoxin.com>
2019-11-07 19:16:30 +0800

    refine
    Signed-off-by: fridayfang <fridayfang@zhaoxin.com>

--- python/cv/ch02_opencv_imread_test.py
+++ python/cv/ch02_opencv_imread_test.py
@@ -15,9 +15,10 @@ import cv2
 print(cv2.__version__, cv2.__doc__)
 
 img = cv2.imread('data/messi5.jpg', cv2.IMREAD_UNCHANGED)  # 包括图像的 alpha 通道
-cv2.imshow('src', img)  
 rows, cols, ch = img.shape
 print('行/高:', rows, '列/宽:', cols, '通道:', ch)
+cv2.imshow('src', img) 
+cv2.moveWindow('src', 0, 0)
 
 img = cv2.resize(img, (640, 480))
 rows, cols, ch = img.shape
@@ -25,16 +26,21 @@ print('行/高:', rows, '列/宽:', cols, '通道:', ch)
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 cv2.imshow('gray', gray)  
+cv2.moveWindow('gray', 0, 0)
+
 ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+cv2.imshow('thresh', thresh)
+cv2.moveWindow('thresh', 0, 0)
 
-cv2.imshow('thresh', thresh)  
-cv2.waitKey(0)
 
-##使用matplotlib来显示
-#plt.imshow(thresh,cmap='gray')
-#plt.show()
+
 
 
 #可以把结果写入jpg文件
 cv2.imwrite('messi5-gray.jpg', gray)
 cv2.imwrite('messi5-thresh.jpg', thresh)
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
