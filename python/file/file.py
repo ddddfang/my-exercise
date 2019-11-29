e0a89d08c1bc8256348391da46e7700de71c3393
fridayfang <fridayfang@zhaoxin.com>
2019-11-29 16:56:38 +0800

    update file.py, pandas .xlsx
    Signed-off-by: fridayfang <fridayfang@zhaoxin.com>

--- python/file/file.py
+++ python/file/file.py
@@ -21,6 +21,11 @@ def get_filename(path, filefilter):
     return result
 
 
+# 直接使用python的glob模块获取某一路径下的所有文件
+import glob
+#files = glob.glob("/home/fang/codes/tmp/my-exercise/python/*.py")
+#files = glob.glob("/home/fang/codes/tmp/my-exercise/python/*/*")
+
 
 #############################################################################################################
 #读取文件,只是一个demo! 作为代码片段
@@ -67,12 +72,20 @@ def write_csv(filename, header, rows):
         f_csv.writerow(header)
         f_csv.writerows(rows)
 
+def read_csv_dict(filename):
+    with open(filename, 'r') as f:
+        f_csv = csv.DictReader(f)
+        result = [] # list 每个成员都是一个 OrderedDict
+        for row in f_csv:
+            result.append(row)
+        return result
+
 def write_csv_dict(filename, header, rows):
     with open(filename, 'w', newline='') as f:
         f_csv = csv.DictWriter(f, header)
         f_csv.writeheader()
         f_csv.writerows(rows)
-#############################################################################################################
+
 
 headers = ['class','name','sex','height','year']
 
@@ -92,28 +105,15 @@ rows = [
 #]
 
 
-abc = read_csv("test.csv")
-write_csv("er.csv", abc[0], abc[1:])
-
-row_header = abc[0]
-rows_dict = []
-for row_i in abc[1:]:
-    #把row_i行的内容组织成一个dict
-    sub_dict = {}
-    for c,col in enumerate(row_i):
-        sub_dict[row_header[c]] = col
-    rows_dict.append(sub_dict)
-
-write_csv_dict("sh.csv",abc[0],rows_dict)
+#############################################################################################################
+import pandas as pd
 
 #############################################################################################################
 
-
-
-if __name__ == "__main__":
+def demo1(): # get_filename and read_file
     filetype = [".py",".txt"]
     rootdir = "/home/fang/codes/tmp/my-exercise/"
-
+    
     print(os.listdir("/home/fang/codes/tmp/")) # 这个和 ls 列出的结果是一样的,返回一个str组成的list
     filenames = get_filename(rootdir, filetype)
     for fi in filenames:
@@ -124,8 +124,62 @@ if __name__ == "__main__":
             print("show line {}:{}".format(i, l), end='')
         print("done.")
 
-    file_lines = ["this is line1.\n", "this is line two~\n"]
-    write_file("abc.txt", file_lines)
+def demo2():    # write_csv and write_csv_dict and read_csv
+    write_csv("test.csv", headers, rows)
+    abc = read_csv("test.csv")
+    print(abc)
+    abc = read_csv_dict("test.csv")
+    print(abc)
+    
+    row_header = abc[0]
+    rows_dict = []
+    for row_i in abc[1:]:
+        #把row_i行的内容组织成一个dict
+        #sub_dict = {}
+        #for c,col in enumerate(row_i):
+        #    sub_dict[row_header[c]] = col
+        #rows_dict.append(sub_dict)
+        rows_dict.append(dict(zip(row_header,row_i)))
+    write_csv_dict("test2.csv",abc[0],rows_dict)
 
+# https://www.cnblogs.com/liulinghua90/p/9935642.html
+def demo3():    # read and write xlsx
+    files = glob.glob("/home/fang/桌面/*.xlsx")
+    print(files)
+
+    #read
+    for i in files:
+        ##df = pd.read_excel(i,sheet_name=0)         #读取第0个sheet
+        #df = pd.read_excel(i,sheet_name="Sheet1")   #按名字读取sheet
+        ##df = pd.read_excel(i,sheet_name="Sheet1",index_col=0,skiprows=3)#第0列是index列,可以不显示,跳过3行
+        ##data = df          # 所有数据, pandas.core.frame.DataFrame 格式
+        #data = df.head()    #只取前5行
+        ##data = df.values   # 返回一个 numpy.ndarray
+        #print("excel data({0}) :{1}.".format(type(data),data))
+
+        with pd.ExcelFile(i) as ef: # 带有解析操作
+            for sh in ef.sheet_names:
+                df = pd.read_excel(ef, sh)
+                #data = df           # 所有数据, pandas.core.frame.DataFrame 格式
+                data = df.values    # 返回一个 numpy.ndarray
+                print("excel data({0}){1} :{2}.".format(type(data), data.shape, data))
+
+                lines = df.index.values
+                print(lines)
+
+                #data = df.ix[10].values    #读取第10行的内容
+                #data = df.iloc[10:12].values    #读取第[10,12)行内容
+                data = df.iloc[10:12, 0:2].values    #读取第[10,12)行,[0,2)列内容
+                print("line10 is {}{}".format(type(data), data))
+
+    # write 一次写入多行, list 的 list 表示一个表格
+    df = pd.DataFrame([[1,2,3,4], [5,6,7,8], [9,10,11,12]], index=[0,1,2], columns=list("ABCD"))
+    df.to_excel("./abc.xlsx")
+
+if __name__ == "__main__":
+
+    #demo1()
+    #demo2()
+    demo3()
 
 
