cc45ed9abce01d65e69812d7e17c12f5d3e7aa44
fridayfang <fridayfang@zhaoxin.com>
2019-12-02 19:35:20 +0800

    refine.
    Signed-off-by: fridayfang <fridayfang@zhaoxin.com>

--- python/mysql_learn.py
+++ python/mysql_learn.py
@@ -8,15 +8,86 @@
 # mysql的命令行客户端.
 
 import pymysql
+import glob
+import pandas as pd
 
-#db = pymysql.connect(host='localhost', port=3307, user="root", password="123456", charset="utf8mb4")
-#cursor = db.cursor()
-#cursor.execute("create database if not exists test_db")
-#db.close()
+config = {
+    "host" : "localhost",
+    "port" : 3307,
+    "user" : "root",
+    "password" : "123456",
+    "charset" : "utf8mb4",
+    "database" : "test_db"
+}
 
-db = pymysql.connect(host='localhost', port=3307, user="root", password="123456", charset="utf8mb4", db="test_db")
+#=============== connect and create database =========================
+db = pymysql.connect(
+    host = config["host"],
+    port = config["port"],
+    user = config["user"],
+    password = config["password"],
+    charset = config["charset"]
+)
+
 cursor = db.cursor()
+cmd = "create database if not exists {}".format(config["database"])
+cursor.execute(cmd)
+db.close()
+
+#================= connect and open database  =======================
+db = pymysql.connect(
+    host = config["host"],
+    port = config["port"],
+    user = config["user"],
+    password = config["password"],
+    charset = config["charset"],
+    db = config["database"]
+)
+
+cursor = db.cursor()
+cmd = '''
+create table if not exists `{}` (
+   `id` int unsigned not null auto_increment,
+   `product_name` char(40) not null,
+   `product_id` int not null,
+   `number` int default 1,
+   `owner` varchar(40) not null,
+   primary key (`id`)
+) engine=InnoDB default charset={};
+'''.format("table_zx",config["charset"])
+cursor.execute(cmd)
+
 cursor.execute("SELECT VERSION()")
 data = cursor.fetchone()
 print ("Database version : {}".format(data))
+
+
+
+cursor.close()
 db.close()
+
+
+
+def demo1():    # read and write xlsx
+    files = glob.glob("/home/fang/桌面/*.xlsx")
+    print(files)
+
+    #read
+    with pd.ExcelFile(files[0]) as ef: # 带有解析操作
+        for sh in ef.sheet_names:
+            df = pd.read_excel(ef, sh)
+            #data = df           # 所有数据, pandas.core.frame.DataFrame 格式
+            data = df.values    # 返回一个 numpy.ndarray
+            print("excel data({0}){1} :{2}.".format(type(data), data.shape, data))
+
+            lines = df.index.values
+            print(lines)
+
+            ##data = df.ix[10].values    #读取第10行的内容
+            ##data = df.iloc[10:12].values    #读取第[10,12)行内容
+            #data = df.iloc[10:12, 0:2].values    #读取第[10,12)行,[0,2)列内容
+            #print("line10 is {}{}".format(type(data), data))
+
+
+if __name__ == "__main__":
+    demo1()
